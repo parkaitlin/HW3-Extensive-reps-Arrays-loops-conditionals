749facde338e2c098d7a4b345a4fd7d7546ea4f5
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-20 16:45:39 -0700

    Commit 3 - Excited Kittens answered

--- js/apps.js
+++ js/apps.js
@@ -3,9 +3,26 @@
 //     console.log(i);
 // };
 
-// Get Even
-for(let i = 0; i <= 200; i++) {
+// // Get Even
+// for(let i = 0; i <= 200; i++) {
+//     if(i % 2 === 0){
+//         console.log(i);
+//     };
+// };
+
+// Excited Kitten
+// write a for loop to evaluate for even numbers
+// if the number is even it should log a string randomly from the sayings array
+// otherwise, it should log "love me, pet me! HSSSSSS!"
+// randomIndex should be inside the loop so that it renews
+
+let sayings = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', 'why does the red dot always get away...'];
+
+for(let i = 0; i <= 20; i++){
+    let randomIndex = Math.floor(Math.random() * sayings.length)
     if(i % 2 === 0){
-        console.log(i);
+        console.log (sayings[randomIndex])
+    } else {
+        console.log("Love me, pet me! HSSSSSS!");
     };
 };
