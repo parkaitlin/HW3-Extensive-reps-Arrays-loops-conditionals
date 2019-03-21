e3d2cefac1f610be8fa9744dd3fd16e49e8ba0dd
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-20 17:57:50 -0700

    Commit 5 - Getting to Know You answered

--- js/apps.js
+++ js/apps.js
@@ -27,15 +27,44 @@
 //     };
 // };
 
-// Fizz Buzz
-for(i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-    console.log("FizzBuzz");
-    } else if(i % 3 === 0){
-        console.log('Fizz');
-    } else if(i % 5 === 0){
-        console.log("Buzz");
-    } else{
-        console.log(i);
-    };
-};
+// // Fizz Buzz
+// for(i = 1; i <= 100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//     console.log("FizzBuzz");
+//     } else if(i % 3 === 0){
+//         console.log('Fizz');
+//     } else if(i % 5 === 0){
+//         console.log("Buzz");
+//     } else{
+//         console.log(i);
+//     };
+// };
+
+// Getting to Know You
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//1
+kenny[0] = "Gameboy";
+console.log(kenny);
+//2
+jimClark[1] = 187;
+console.log(jimClark);
+//3
+ryan[2] = "Gotham City";
+console.log(ryan);
+//4
+reuben.pop();
+console.log(reuben);
+reuben[2] = "Chicago";
+console.log(reuben);
+//5
+jimHaff.pop();
+jimHaff.push("Chicago", "New York", "Seoul");
+console.log(jimHaff);
+//6
+jimHaff.splice(2,1);
+console.log(jimHaff);
